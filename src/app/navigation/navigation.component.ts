93fb311471762acff4087ce47dea5dffaca77e61
aarsh64 <aarsh64@gmail.com>
2019-06-25 19:19:51 +0530

    minor modification in new functions 1.4 (navtigation-bar)

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ViewChild, TemplateRef, ElementRef } from '@angular/core';
+import { Overlay } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-navigation',
@@ -6,18 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
+  templatePortal: any;
+  backlogTemp: any;
+  overlayRef: any;
+@ViewChild('usersTemplate',{static:false})
+usersTemplate:TemplateRef<any>;
+@ViewChild('notifications',{static:false})
+notifications:TemplateRef<any>;
+@ViewChild('notification',{static:false})
+notification:ElementRef<any>;
+  constructor(private overlay: Overlay,private _viewContainerRef: ViewContainerRef) { }
 
   ngOnInit() {
   }
+
+  users(){
+    const positionStrategy=this.overlay.position().global().centerHorizontally().centerVertically();
+    this.templatePortal=new TemplatePortal(this.usersTemplate,this._viewContainerRef); 
+    this.overlayRef=this.overlay.create({
+       positionStrategy  
+    });
+    this.overlayRef.attach(this.templatePortal); 
+  }
   bellButton(){
-    console.log('bell button working....');
+    const positionStrategy=this.overlay.position().connectedTo(this.notification,{originX:'start',originY:'bottom'},{overlayX:'end',overlayY:'top'});
+    this.templatePortal=new TemplatePortal(this.notifications,this._viewContainerRef); 
+    this.overlayRef=this.overlay.create({
+       positionStrategy  
+    });
+    this.overlayRef.attach(this.templatePortal); 
+
   }
 invite(){
   console.log('invite button....');
 }
 search(){
   console.log('searching working....');
+}
+close(){
+  this.overlayRef.dispose();
 }
 }
